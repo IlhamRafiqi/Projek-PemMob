9277b080065f99199663d4246f46e7693f9c9f96
IlhamRafiqi <ilhamrafiqi01@gmail.com>
2025-09-28 15:21:31 +0700

    Perubahan Terbaru

--- app/src/main/java/com/example/sejenakapps/MainActivity.kt
+++ app/src/main/java/com/example/sejenakapps/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.sejenakapps
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        // Load index.html dari assets
+        webView.loadUrl("file:///android_asset/index.html")
     }
 }
